0a5c7903af55f7a0fe791ac25e12bd2515f2c94e
Fabian Große <saphareas@googlemail.com>
2019-01-17 13:51:31 +0100

    Fix for Gnome 3.30+

--- extension.js
+++ extension.js
@@ -6,20 +6,29 @@ const Shell = imports.gi.Shell;
 const GLib = imports.gi.GLib;
 const PopupMenu = imports.ui.popupMenu;
 const Gettext = imports.gettext;
-
 const _ = Gettext.gettext;
 
 let item, userMenu;
 
+function _getUserMenu() {
+    if (Main.panel._statusArea) {
+        return Main.panel._statusArea.userMenu.menu;
+    } else if (Main.panel.statusArea.userMenu) {
+        return Main.panel.statusArea.userMenu.menu;
+    } else {
+        return Main.panel.statusArea.aggregateMenu.menu;
+    }
+}
+
 function _onAdvancedSettingsActivate() {
     Main.overview.hide();
-    let app = Shell.AppSystem.get_default().lookup_app('gnome-tweak-tool.desktop');
+    let app = Shell.AppSystem.get_default().lookup_app('org.gnome.tweaks.desktop');
     app.activate();
 }
 
 function init(extensionMeta) {
     Gettext.bindtextdomain("advanced-settings-in-usermenu@nuware.ru", GLib.build_filenamev([extensionMeta.path, 'locale']));
-    userMenu = Main.panel._statusArea.userMenu.menu;
+    userMenu = _getUserMenu();
 }
 
 function enable() {
